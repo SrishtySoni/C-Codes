679ffbe8f6358e5b2db1d2ef43ac6a9f35d7557e
SrishtySoni <052000srishtysoni@gmail.com>
2021-10-18 20:07:00 +0530

    18thoct

---
+++ pointers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float agee=30.5;
+    int salary=1500;
+    printf("address of agee=%p\n",&agee);
+    printf("address of salary=%p\n",&salary);
+    return 0;
+}
