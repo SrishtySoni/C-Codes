7c2fa8a8e50398aa0700308caea912f004ef45dc
SrishtySoni <052000srishtysoni@gmail.com>
2021-10-19 10:15:24 +0530

    19thoct

---
+++ primeno.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter a number\n");
+    scanf("%d",&n);
+    int isprime[n+1];
+    for(int i=0;i<=n;i++)
+        isprime[i]=1;
+    isprime[0]=isprime[1]=0;
+    for(int i=2;i<n;i++)
+    {
+        if(isprime[i]==1)
+        {
+            for(int j=2;j*i<=n;i++)
+            {
+                isprime[i*j]=0;
+            }
+        }
+    }
+    for(int i=0;i<=n;i++)
+      printf("%d %d\n",i,isprime[i]);
+      
+
+    return 0;
+}
